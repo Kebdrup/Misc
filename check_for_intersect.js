91f4192f83d91bdd092fc430efad3e992ad5422f
Kristian Ebdrup <ke@onlinepos.dk>
2017-02-22 16:40:24 +0100

    WIP

--- check_for_intersect.js
+++ check_for_intersect.js
@@ -1,13 +1,15 @@
 var buttons = 
 	[{
-		"index": 1,
+		"function": "Kontant",
+		"text": "hello",
 		"grids": [
 			[2, 2],
 			[2, 3]
 		]
 	},
 	{
-		"index": 2,
+		"function": "Dankort",
+		"text": "world",
 		"grids": [
 			[3, 4],
 			[3, 5],
@@ -27,18 +29,19 @@ var grids =
 check_for_intersect(grids, buttons);
 
 function check_for_intersect(grids, buttons){
-	for(var i = 0; i<buttons.length; i++){
+	for(var i = 0; i<buttons.length; i++){//foreach button
 		var btn = buttons[i];
 		var grids_to_check = btn.grids;
-		for(var j = 0; j<grids_to_check.length; j++){
+		for(var j = 0; j<grids_to_check.length; j++){//foreach grid
 			var grid = grids_to_check[j];
 			var x = grid[0];
 			var y = grid[1];
-			for(var h = 0; h<grids.length; h++){
+			for(var h = 0; h<grids.length; h++){//forech new grid
 				var grid_new = grids[h];
 				var x_new = grid_new[0];
 				var y_new = grid_new[1];
 				if(x == x_new && y == y_new){
+					//there is intersection
 					alert("x: "+x+", y: "+y);
 				}
 			}
