2a59b36d7e2fc120a22ddc35af1cd1302d6c6414
Kristian Ebdrup <ke@onlinepos.dk>
2017-03-22 17:06:55 +0100

    JSON sersc

---
+++ button.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buttons!</title>
+	<link rel="stylesheet" href="button.css" type="text/css">
+</head>
+<body>
+<?php
+var_dump(get_defined_vars());
+?>
+<span>This is a button</span>
+
+<div class="button">
+	<img class="remove-button" src="close2.png"></button>
+	<div class="text-wrapper">
+		<span class="button-text">Kontant</span>
+	</div>
+</div>
+
+</body>
+</html>
--- tabs.js
+++ tabs.js
@@ -19,4 +19,29 @@ $(document).ready(function(){
 		set_tab(this);
 	});
 	set_tab($("#tab-1")[0]);
+
+	//load and layout buttons
+	$.ajax({
+		url: "buttons.json",
+		success: function(tabs){
+			//alert(JSON.stringify(json));
+
+			var html = "";
+			$.each(tabs, function(){
+				//alert(JSON.stringify(this.buttons));
+				html_tab = '<div class="tab" id="tab-5"><span class="tab-text">'+this.tab+'</span></div>';
+				var html_buttons = "";
+				$.each(this.buttons, function(){
+					//generates button html
+					html_buttons += '<div class="button"><span class="button-text">'+this.name+'</span></div>';
+				});
+				html_content = '<div id="content-5" class="tab-content-actual">'+html_buttons+'/<div>';
+				//append to html
+				$(".menu-tabs").append(html_tab);
+				$(".tab-content").append(html_content);
+			});
+
+			//$(".tab-content").html(data)
+		}
+	});
 });
